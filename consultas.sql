df596a1ec603f0a7434415cfa0e359b9862f8452
Rayssa Priscila <rayssapriscilasn12@gmail.com>
2026-04-09 07:45:30 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -1,7 +1,21 @@
+-- Consultas básicas 
 select nome_disc as "Nome da disciplina"
 from disciplina;
 
 select * from usuario;
 
+-- Consultas com WHERE
 select enunciado, dificuldade from questao
 where dificuldade = 'fácil'
+
+select meta from relatorio
+where meta > 10;
+
+select cod_usuario, nome from usuario
+where nome like 'A%';
+
+-- Ordenação dos dados 
+select cod_usuario, nome from usuario
+order by nome;
+
+
