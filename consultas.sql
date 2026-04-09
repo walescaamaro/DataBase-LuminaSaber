991b5030960f647afea60a8ccda23a2d6126e8a3
Walesca Amaro Rodrigues <walescaamaro.dev@gmail.com>
2026-04-09 08:09:33 -0300

    Add queries to retrieve user and history data

--- consultas.sql
+++ consultas.sql
@@ -27,4 +27,29 @@ FROM usuario u
 INNER JOIN anotacao an 
     ON u.cod_usuario = an.cod_usuario
 
+SELECT 
+    u.nome,
+    q.enunciado,
+    d.nome_disc,
+    h.status
+FROM historico h
+INNER JOIN usuario u 
+    ON h.cod_usuario = u.cod_usuario
+INNER JOIN questao q 
+    ON h.cod_quest = q.cod_quest
+INNER JOIN disciplina d 
+    ON q.cod_disc = d.cod_disc;
+
+SELECT u.nome, d.nome_disc, h.status
+FROM historico h
+INNER JOIN usuario u 
+    ON h.cod_usuario = u.cod_usuario
+INNER JOIN questao q 
+    ON h.cod_quest = q.cod_quest
+INNER JOIN disciplina d 
+    ON q.cod_disc = d.cod_disc
+WHERE 
+    h.status = 'Acertou'
+    AND d.nome_disc = 'Matemática';
+
 
