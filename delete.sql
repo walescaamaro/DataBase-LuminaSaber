9b8c18185182eaff4e99cf148f2ac3d861db93c0
Walesca Amaro Rodrigues <walescaamaro.dev@gmail.com>
2026-04-23 07:51:14 -0300

    Update delete.sql

--- delete.sql
+++ delete.sql
@@ -6,6 +6,7 @@ DELETE FROM suporte WHERE cod_suporte = 11456;
 --  Exclusão com condições: 
 DELETE FROM suporte WHERE tipo_problema = 'Dúvidas' AND cod_suporte < 30000;
 
-DELETE FROM relatorio 
-WHERE percentual_acertos < 50;
+DELETE FROM relatorio WHERE percentual_acertos < 50;
+
+-- Exclusões que façam sentido no sistema:
 
