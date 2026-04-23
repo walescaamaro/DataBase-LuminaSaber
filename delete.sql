b90dc82a88c6e5d56c71240d23add343d1d71c7e
Walesca Amaro Rodrigues <walescaamaro.dev@gmail.com>
2026-04-23 07:39:38 -0300

    Add conditional delete statements to delete.sql

--- delete.sql
+++ delete.sql
@@ -3,3 +3,9 @@ DELETE FROM anotacao WHERE cod_usuario = 59384;
 
 DELETE FROM suporte WHERE cod_suporte = 11456;
 
+--  Exclusão com condições: 
+DELETE FROM suporte WHERE tipo_problema = 'Dúvidas' AND cod_suporte < 30000;
+
+DELETE FROM relatorio 
+WHERE percentual_acertos < 50;
+
